f530c3aa00bd8ae7cf840e84b01475223a7b60bf
Tyson George <tgeorge06@gmail.com>
2026-02-14 14:22:01 -0500

    Update sha256 for v0.1.0

--- Formula/lazyide.rb
+++ Formula/lazyide.rb
@@ -2,7 +2,7 @@ class Lazyide < Formula
   desc "Lightweight terminal-native IDE built with Rust and ratatui"
   homepage "https://github.com/tgeorge06/lazyide"
   url "https://github.com/tgeorge06/lazyide/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "1ccff3eaa67b657842fe8b92637bdb137bd118af5e0096e29af993f8d5dae5a0"
+  sha256 "447ddabdc909623f060a65711331574e80e3bb46bcb1b9f49f13fac5fb4d90a0"
   license "MIT"
 
   depends_on "rust" => :build
