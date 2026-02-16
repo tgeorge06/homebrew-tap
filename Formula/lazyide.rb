25e385b7f164357feac103ba1d0fa50525fc9510
Tyson George <tgeorge06@gmail.com>
2026-02-16 16:44:51 -0500

    Update lazyide to v0.3.4

--- Formula/lazyide.rb
+++ Formula/lazyide.rb
@@ -1,16 +1,16 @@
 class Lazyide < Formula
   desc "Lightweight terminal-native IDE built with Rust and ratatui"
   homepage "https://github.com/tgeorge06/lazyide"
-  version "0.3.3"
+  version "0.3.4"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/tgeorge06/lazyide/releases/download/v0.3.3/lazyide-macos-aarch64.tar.gz"
-      sha256 "403a7949d9a680d0d6cad8d5ca36e103e9c0c3209a513a27100a17b9893a9333"
+      url "https://github.com/tgeorge06/lazyide/releases/download/v0.3.4/lazyide-macos-aarch64.tar.gz"
+      sha256 "d9a75417523a46eb7b5f69de09008791d328263593f4167bdec20178ccc6a6e0"
     else
-      url "https://github.com/tgeorge06/lazyide/releases/download/v0.3.3/lazyide-macos-x86_64.tar.gz"
-      sha256 "ebf2e7daf9b9da30d88b2bfb43ef248e0d2b6e960b4e32e76360ff7ef53bb07b"
+      url "https://github.com/tgeorge06/lazyide/releases/download/v0.3.4/lazyide-macos-x86_64.tar.gz"
+      sha256 "4e2af09b6567cf0b0d34d41b5a0321f7a0dd7e8143aea2dd7a6dc732e9fae72e"
     end
   end
 
